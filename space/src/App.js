fa2275316b0d65e19c564ec2ad837d2d4481d73c
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2021-11-19 22:32:30 +0100

    intro

--- space/src/App.js
+++ space/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Galaxy from "./components/galaxy";
+import Intro from './components/intro'
 
 function App() {
   return (
     <div className="App">
+      <Intro />
       <Galaxy />
+      
     </div>
   );
 }
---
+++ space/src/components/intro/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styl.css'
+
+ function Intro() {
+    return (
+        <div class="container">
+            <svg width="100%" height="100%">
+                <text x="50%" y="60%" text-anchor="middle">Galaxy Simulator</text>
+            </svg>
+        </div>
+    )
+}
+
+
+export default Intro;
