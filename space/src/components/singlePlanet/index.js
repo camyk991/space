63940f326d3384f4fe5d3332516016d728535e2d
Paty1355 <94170413+Paty1355@users.noreply.github.com>
2021-11-20 02:06:10 +0100

    style

--- space/src/components/singlePlanet/index.js
+++ space/src/components/singlePlanet/index.js
@@ -3,27 +3,22 @@ import {useEffect} from 'react';
 
 
 const SinglePlanet = ({planet}) => {
-
-    
         return (
             <div className="singlePlanetContainer">
                 <h3 className="planet-heading">{planet.englishName}</h3> 
                 <div className="data-wrapper">
-                    <span className="row">Alternative name: {planet.alternativeName ? planet.alternativeName : '-'} </span>
-                    <span className="row">Discovered by: {planet.discoveredBy}</span>
-                    <span className="row">Discovery date: {planet.discoveryDate}</span>
-                    <span className="row">Mass: {planet.mass.massValue}<sup>{planet.mass.massExponent}</sup> kg</span>
-                    <span className="row">Density: {planet.density} g/cm<sup>3</sup></span>
-                    <span className="row">Equatorial radius: {planet.equaRadius} km</span>
-                    <span className="row">Flattening: {planet.flattening} </span>
-                    <span className="row">Sidereal rotation: {planet.sideralRotation} h</span>
-                    <span className="row">Axial tilt: {planet.axialTilt} </span>
-                    <span className="row">Moons: {planet.moons ? planet.moons.length : "-"} </span>
-                </div>
+                    <span className="row"><span className="p">Alternative name:</span> {planet.alternativeName ? planet.alternativeName : '-'} </span>
+                    <span className="row"><span className="p">Discovered by:</span> {planet.discoveredBy ? planet.discoveredBy : '-'}</span>
+                    <span className="row"><span className="p">Discovery date:</span> {planet.discoveryDate ? planet.discoveryDate : '-'}</span>
+                    <span className="row"><span className="p">Mass:</span> {planet.mass.massValue ? planet.mass.massValue : '-'}<sup>{planet.mass.massExponent}</sup> kg</span>
+                    <span className="row"><span className="p">Density:</span> {planet.density ? planet.density : '-'} g/cm<sup>3</sup></span>
+                    <span className="row"><span className="p">Equatorial radius:</span> {planet.equaRadius ? planet.equaRadius : '-'} km</span>
+                    <span className="row"><span className="p">Flattening: </span>{planet.flattening ? planet.flattening: '-'} </span>
+                    <span className="row"><span className="p">Sidereal rotation:</span> {planet.sideralRotation ? planet.sideralRotation : '-'} h</span>
+                    <span className="row"><span className="p">Axial tilt:</span> {planet.axialTilt ? planet.axialTilt : '-'} </span>
+                    <span className="row"><span className="p">Moons: </span>{planet.moons ? planet.moons.length : "-"} </span>
+                </div> 
             </div>
-            
-            
-
         )
    
 }
